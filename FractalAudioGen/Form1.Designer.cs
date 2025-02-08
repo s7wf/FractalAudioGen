a7a1d88775e0545719f92faea90af101854463ad
jon <mail@example.com>
2025-02-07 23:39:35 -0800

    Data bind frequency intent combo

--- FractalAudioGen/Form1.Designer.cs
+++ FractalAudioGen/Form1.Designer.cs
@@ -11,6 +11,7 @@
         private System.Windows.Forms.Label lblBinauralOffset;
         private System.Windows.Forms.TrackBar trackBinauralOffset;
         private System.Windows.Forms.ComboBox cmbWaveType;
+        private System.Windows.Forms.Label lblWaveType;
 
         /// <summary>
         /// Required method for Designer support - do not modify
@@ -19,48 +20,45 @@
         private void InitializeComponent()
         {
             btnPlay = new System.Windows.Forms.Button();
-
-
-
             btnStop = new System.Windows.Forms.Button();
             lblFrequency = new System.Windows.Forms.Label();
             cmbHealingFrequencies = new System.Windows.Forms.ComboBox();
             lblBinauralOffset = new System.Windows.Forms.Label();
             trackBinauralOffset = new System.Windows.Forms.TrackBar();
             cmbWaveType = new System.Windows.Forms.ComboBox();
-            waveType = new System.Windows.Forms.Label();
+            lblWaveType = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)trackBinauralOffset).BeginInit();
             SuspendLayout();
-            // 
+            //
             // btnPlay
-            // 
+            //
             btnPlay.Location = new System.Drawing.Point(20, 20);
             btnPlay.Name = "btnPlay";
             btnPlay.Size = new System.Drawing.Size(80, 30);
             btnPlay.TabIndex = 0;
             btnPlay.Text = "Play";
             btnPlay.Click += BtnPlay_Click;
-            // 
+            //
             // btnStop
-            // 
+            //
             btnStop.Location = new System.Drawing.Point(110, 20);
             btnStop.Name = "btnStop";
             btnStop.Size = new System.Drawing.Size(80, 30);
             btnStop.TabIndex = 1;
             btnStop.Text = "Stop";
             btnStop.Click += BtnStop_Click;
-            // 
+            //
             // lblFrequency
-            // 
+            //
             lblFrequency.AutoSize = true;
             lblFrequency.Location = new System.Drawing.Point(20, 70);
             lblFrequency.Name = "lblFrequency";
             lblFrequency.Size = new System.Drawing.Size(79, 20);
             lblFrequency.TabIndex = 2;
             lblFrequency.Text = "Frequency:";
-            // 
+            //
             // cmbHealingFrequencies
-            // 
+            //
             cmbHealingFrequencies.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             cmbHealingFrequencies.Items.AddRange(new object[] { "174 Hz - Pain Relief", "285 Hz - Tissue Healing", "396 Hz - Fear Reduction", "417 Hz - Negative Energy", "432 Hz - Natural Harmony", "528 Hz - DNA Repair", "639 Hz - Love & Connection", "741 Hz - Detox & Immunity", "852 Hz - Intuition Boost", "963 Hz - Pineal Gland" });
             cmbHealingFrequencies.Location = new System.Drawing.Point(120, 65);
@@ -68,18 +66,18 @@
             cmbHealingFrequencies.Size = new System.Drawing.Size(200, 28);
             cmbHealingFrequencies.TabIndex = 3;
             cmbHealingFrequencies.SelectedIndexChanged += CmbHealingFrequencies_SelectedIndexChanged;
-            // 
+            //
             // lblBinauralOffset
-            // 
+            //
             lblBinauralOffset.AutoSize = true;
             lblBinauralOffset.Location = new System.Drawing.Point(20, 120);
             lblBinauralOffset.Name = "lblBinauralOffset";
             lblBinauralOffset.Size = new System.Drawing.Size(144, 20);
             lblBinauralOffset.TabIndex = 4;
             lblBinauralOffset.Text = "Binaural Offset: 0 Hz";
-            // 
+            //
             // trackBinauralOffset
-            // 
+            //
             trackBinauralOffset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
             trackBinauralOffset.Location = new System.Drawing.Point(20, 140);
             trackBinauralOffset.Maximum = 20;
@@ -87,37 +85,37 @@
             trackBinauralOffset.Size = new System.Drawing.Size(339, 56);
             trackBinauralOffset.TabIndex = 5;
             trackBinauralOffset.ValueChanged += TrackBinauralOffset_Scroll;
-            // 
+            //
             // cmbWaveType
-            // 
+            //
             cmbWaveType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             cmbWaveType.Location = new System.Drawing.Point(121, 201);
             cmbWaveType.Name = "cmbWaveType";
             cmbWaveType.Size = new System.Drawing.Size(200, 28);
             cmbWaveType.TabIndex = 6;
             cmbWaveType.SelectedIndexChanged += CmbWaveType_SelectedIndexChanged;
-            // 
-            // waveType
-            // 
-            waveType.Location = new System.Drawing.Point(17, 198);
-            waveType.Name = "waveType";
-            waveType.Size = new System.Drawing.Size(98, 32);
-            waveType.TabIndex = 7;
-            waveType.Text = "Wave Type:";
-            waveType.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
+            //
+            // lblWaveType
+            //
+            lblWaveType.Location = new System.Drawing.Point(17, 198);
+            lblWaveType.Name = "lblWaveType";
+            lblWaveType.Size = new System.Drawing.Size(98, 32);
+            lblWaveType.TabIndex = 7;
+            lblWaveType.Text = "Wave Type:";
+            lblWaveType.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            //
             // Form1
-            // 
+            //
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(371, 257);
-            Controls.Add(waveType);
             Controls.Add(btnPlay);
             Controls.Add(btnStop);
             Controls.Add(lblFrequency);
             Controls.Add(cmbHealingFrequencies);
             Controls.Add(lblBinauralOffset);
             Controls.Add(trackBinauralOffset);
+            Controls.Add(lblWaveType);
             Controls.Add(cmbWaveType);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -126,7 +124,5 @@
             ResumeLayout(false);
             PerformLayout();
         }
-
-        private System.Windows.Forms.Label waveType;
     }
 }
--- FractalAudioGen/Form1.cs
+++ FractalAudioGen/Form1.cs
@@ -9,6 +9,20 @@ namespace FractalAudioGen
         private readonly WaveOutEvent waveOut;
         private readonly FractalWaveProvider waveProvider;
 
+        private readonly IReadOnlyDictionary<int, string> supportedFrequencies = new Dictionary<int, string>
+        {
+            { 174, "Pain Relief" },
+            { 285, "Tissue Healing" },
+            { 396, "Fear Reduction" },
+            { 417, "Negative Energy" },
+            { 432, "Natural Harmony" },
+            { 528, "DNA Repair" },
+            { 639, "Love & Connection" },
+            { 741, "Detox & Immunity" },
+            { 852, "Intuition Boost" },
+            { 963, "Pineal Gland" },
+        };
+
         public Form1()
         {
             // Perform app init
@@ -20,8 +34,27 @@ namespace FractalAudioGen
             InitializeComponent();
 
             // Bind controls
-            cmbHealingFrequencies.SelectedIndex = 0;
+            cmbHealingFrequencies.DataSource = new BindingSource(supportedFrequencies, null);
+            cmbHealingFrequencies.DisplayMember = "Value";
+            cmbHealingFrequencies.ValueMember = "Key";
+            cmbHealingFrequencies.FormattingEnabled = true;
+            // Format list entries by listing both Hz and intent
+            cmbHealingFrequencies.Format += (_, args) =>
+            {
+                var listItem = (KeyValuePair<int, string>)args.ListItem!;
+                args.Value = $"{listItem.Key} Hz - {listItem.Value}";
+            };
+
+            // cmbHealingFrequencies.FormattingEnabled = true;
+            // cmbHealingFrequencies.FormatString = "{0} Hz - {1}";
+
+            // Set our default to DNA Repair
+            cmbHealingFrequencies.SelectedIndex = 5;
+
             cmbWaveType.DataSource = Enum.GetValues<FractalWaveType>();
+
+            // Stop is disabled by default, nothing is playing yet
+            btnStop.Enabled = false;
         }
 
         private void BtnPlay_Click(object sender, EventArgs e)
@@ -29,6 +62,8 @@ namespace FractalAudioGen
             if (waveOut.PlaybackState != PlaybackState.Playing)
             {
                 waveOut.Play();
+                btnPlay.Enabled = false;
+                btnStop.Enabled = true;
             }
         }
 
@@ -37,24 +72,24 @@ namespace FractalAudioGen
             if (waveOut.PlaybackState == PlaybackState.Playing)
             {
                 waveOut.Stop();
+                btnPlay.Enabled = true;
+                btnStop.Enabled = false;
             }
         }
 
         private void CmbHealingFrequencies_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cmbHealingFrequencies.SelectedItem is string selection)
-            {
-                UpdateFrequency(selection);
-            }
+            // When the control is first initialized, bindings are not set up yet - allow early term
+            if (cmbHealingFrequencies.SelectedValue is not int selectedFrequency)
+                return;
+
+            waveProvider.Frequency = selectedFrequency;
         }
 
         private void TrackBinauralOffset_Scroll(object sender, EventArgs e)
         {
-            if (trackBinauralOffset != null && lblBinauralOffset != null)
-            {
-                waveProvider.BinauralOffset = trackBinauralOffset.Value;
-                lblBinauralOffset.Text = $"Binaural Offset: {trackBinauralOffset.Value} Hz";
-            }
+            waveProvider.BinauralOffset = trackBinauralOffset.Value;
+            lblBinauralOffset.Text = $"Binaural Offset: {trackBinauralOffset.Value} Hz";
         }
 
         private void CmbWaveType_SelectedIndexChanged(object sender, EventArgs e)
@@ -62,48 +97,6 @@ namespace FractalAudioGen
             if (cmbWaveType.SelectedItem is FractalWaveType waveType)
             {
                 waveProvider.WaveType = waveType;
-            }
-        }
-
-        private void UpdateFrequency(string selection)
-        {
-            if (string.IsNullOrEmpty(selection)) return;
-
-            switch (selection)
-            {
-                case "174 Hz - Pain Relief":
-                    waveProvider.Frequency = 174;
-                    break;
-                case "285 Hz - Tissue Healing":
-                    waveProvider.Frequency = 285;
-                    break;
-                case "396 Hz - Fear Reduction":
-                    waveProvider.Frequency = 396;
-                    break;
-                case "417 Hz - Negative Energy":
-                    waveProvider.Frequency = 417;
-                    break;
-                case "432 Hz - Natural Harmony":
-                    waveProvider.Frequency = 432;
-                    break;
-                case "528 Hz - DNA Repair":
-                    waveProvider.Frequency = 528;
-                    break;
-                case "639 Hz - Love & Connection":
-                    waveProvider.Frequency = 639;
-                    break;
-                case "741 Hz - Detox & Immunity":
-                    waveProvider.Frequency = 741;
-                    break;
-                case "852 Hz - Intuition Boost":
-                    waveProvider.Frequency = 852;
-                    break;
-                case "963 Hz - Pineal Gland":
-                    waveProvider.Frequency = 963;
-                    break;
-                default:
-                    waveProvider.Frequency = 528; // Default to 528 Hz
-                    break;
             }
         }
     }
